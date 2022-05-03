b50fd40bc2c1c9f407a875861b11addd0a268e3e
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-05-04 02:11:54 +0300

    Добавил реализацию класса для наших моделей
    Проверить пока не могу, но сам sql запрос который для qs используется - рабочий, скрин приложил

--- movies/api/v1/views.py
+++ movies/api/v1/views.py
@@ -4,23 +4,27 @@ from django.http import JsonResponse
 from django.views.generic.list import BaseListView
 from django.views.generic.detail import BaseDetailView
 
-from movies.models import FilmWork, Person, PersonFilmWork
+from movies.models import FilmWork, Person, PersonFilmWork, GenreFilmWork, Genre
 
+"""Код ниже возвращает результат"""
+# filmworks_genres = GenreFilmWork.objects.all().select_related('film_work', 'genre')[:3]
+# for filmwork_genre in filmworks_genres:
+#     print(filmwork_genre.film_work.title, filmwork_genre.genre.name)
+
+"""ниже класс как выводить даныне с учетом скрипта выше  https://disk.yandex.ru/i/IeDdbFAVAkeUjw"""
 
 class MoviesApiMixin:
     model = FilmWork
     http_method_names = ['get']
 
     def get_queryset(self):
-        qs = FilmWork.objects.prefetch_related(
+        qs = GenreFilmWork.objects.select_related('film_work', 'genre'
             'genres', 'persons'
         ).values(
-            'id', 'title', 'description', 'creation_date', 'rating', 'type'
+            'id', 'film_work'
         ).annotate(
-            genres=ArrayAgg('genres__name', distinct=True),
-            actors=self._aggregate_person(role=PersonRole.ACTOR),
-            directors=self._aggregate_person(role=PersonRole.DIRECTOR),
-            writers=self._aggregate_person(role=PersonRole.WRITER)
+            genres=ArrayAgg('genre__name', distinct=True),
+            rating=ArrayAgg('film_work__rating', distinct=True)
         )
         return qs
 
@@ -29,6 +33,30 @@ class MoviesApiMixin:
 
     def _aggregate_person(self, role):
         return ArrayAgg('persons__full_name', distinct=True, filter=Q(filmworkperson__role=role))
+
+"""это класс второго участника - он закомментирован"""
+# class MoviesApiMixin:
+#     model = FilmWork
+#     http_method_names = ['get']
+#
+#     def get_queryset(self):
+#         qs = FilmWork.objects.prefetch_related(
+#             'genres', 'persons'
+#         ).values(
+#             'id', 'title', 'description', 'creation_date', 'rating', 'type'
+#         ).annotate(
+#             genres=ArrayAgg('genres__name', distinct=True),
+#             actors=self._aggregate_person(role=PersonRole.ACTOR),
+#             directors=self._aggregate_person(role=PersonRole.DIRECTOR),
+#             writers=self._aggregate_person(role=PersonRole.WRITER)
+#         )
+#         return qs
+#
+#     def render_to_response(self, context, **response_kwargs):
+#         return JsonResponse(context)
+#
+#     def _aggregate_person(self, role):
+#         return ArrayAgg('persons__full_name', distinct=True, filter=Q(filmworkperson__role=role))
 
 
 class MoviesListApi(MoviesApiMixin, BaseListView):
