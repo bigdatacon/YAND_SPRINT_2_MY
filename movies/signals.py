65ade40ff5196f981a2d58cb01c0970ad67460bb
root <root@ovz4.j54523970.pv29m.vps.myjino.ru>
2022-05-10 19:55:30 +0300

    Fix signal processing

--- movies/signals.py
+++ movies/signals.py
@@ -5,5 +5,5 @@ from django.dispatch import receiver
 
 @receiver(post_save, sender='movies.Person')
 def congratulatory(sender, instance, created, **kwargs):
-    if created and instance.created_at == datetime.date.today():
+    if created and instance.created_at.date() == datetime.date.today():
         print(f"У {instance.full_name} сегодня день рождения! 🥳")
